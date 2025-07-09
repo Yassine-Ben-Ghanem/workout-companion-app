c5a59f503b988f49b9db48b6cb2cf2bb682ce7ed
Yassine BEN GHANEM <benghanem1999@gmail.com>
2025-07-09 12:08:25 +0100

    Refactor environment variable

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,5 +1,4 @@
 import Config from 'react-native-config';
-import { WEATHER_API_KEY, WEATHER_API_URL } from '@env';
 
 // Define the environment variables types
 export interface Env {
@@ -11,12 +10,8 @@ export interface Env {
 // Create a strongly typed config object
 const env: Env = {
   API_URL: Config.API_URL || '',
-  // Use Config as fallback for non-test environments
-  WEATHER_API_KEY: WEATHER_API_KEY || Config.WEATHER_API_KEY || '',
-  WEATHER_API_URL:
-    WEATHER_API_URL ||
-    Config.WEATHER_API_URL ||
-    'https://api.weatherapi.com/v1',
+  WEATHER_API_KEY: Config.WEATHER_API_KEY || '',
+  WEATHER_API_URL: Config.WEATHER_API_URL || 'https://api.weatherapi.com/v1',
 };
 
 export default env;
